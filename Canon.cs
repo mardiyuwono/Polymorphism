7c49104e4e82db30a55b94d4eb490034b0ca0656
mardiyuwono <61926735+mardiyuwono@users.noreply.github.com>
2020-05-07 21:40:56 +0700

    Polymorphism Menggunakan Inheritance/Pewarisan

---
+++ Canon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class Canon : PrinterWindows
+    {
+        public override void Show()
+        {
+            Console.WriteLine("\nEpson display dimension : 9.5*12");
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Epson printer printing....");
+        }
+    }
+}
